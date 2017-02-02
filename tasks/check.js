22ad01da17dec62a84948f24a2f9e84896ea72af
Scott Hardy <scott.the.hardy@gmail.com>
2017-02-01 16:53:56 -0800

    price history graph. closes #5

--- tasks/check.js
+++ tasks/check.js
@@ -37,7 +37,7 @@ const COOLDOWN = 3 * 24 * 60 * 60; // max one text every 3 days
 
         // process alerts
         await alert.getLatestPrice();
-        await redis.setAsync(alert.key(), alert.value);
+        await redis.setAsync(alert.key(), alert.toJSON());
         const less = alert.price - alert.latestPrice;
         if (less > 0) {
           console.log(`${flight} dropped $${less} to $${alert.latestPrice}`);
